7d60393ee581d5597cbc0d492d9cafcd51fa59d9
Diane Hubau <diane.hubau@gmail.com>
2021-05-26 15:39:54 +0200

    Setup i18n without devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :cities, only: [:show] do
-    resources :events, only: [:new, :create, :show] do
-      resources :participants, only: [:create, :destroy]
+  scope '(:locale)', locale: /fr/ do
+    root to: 'pages#home'
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    resources :cities, only: [:show] do
+      resources :events, only: [:new, :create, :show] do
+        resources :participants, only: [:create, :destroy]
+      end
     end
   end
 end
